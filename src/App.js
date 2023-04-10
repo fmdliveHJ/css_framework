9bf39f308b6b554dc21e209b071e3df74b6ae4de
정현진 <gdalove@naver.com>
2023-04-10 14:34:32 +0900

    Fix 폴더 구조 변경

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Router from './pages/Router';
 import './style/reset.css';
 import './style/guide.css';
-import './style/style.css';
-
+// import './style/style.css';
+import 'baro_css/src/style/style.css';
 function App() {
   return (
     <div className='App'>
